f5274d788ce3840366294bf3d917ff2d386b91b3
bbenyo <bbenyo@gmail.com>
2018-07-04 14:51:03 -0400

    Implement statistics handler using FileStats/DirectoryStats

--- src/main/java/util/handlers/Statistics.java
+++ src/main/java/util/handlers/Statistics.java
@@ -1,26 +1,26 @@
 package util.handlers;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
 
 import util.struct.DirectoryStats;
 import util.struct.FileTypeStats;
 import util.struct.MediaFile;
 
 public class Statistics extends MediaHandler {
-
+	static private Logger logger = Logger.getLogger(Statistics.class.getName());
+	
 	// Store all computed statistics
-	protected ArrayList<DirectoryStats> directoryStats;
+	protected HashMap<File, DirectoryStats> directoryStats;
 	
 	// Aggregate statistics for all directories
 	FileTypeStats rootStats; 
-	
-	// Directories we're currently working on
-	
-	
+		
 	public Statistics() {
 		super();
-		directoryStats = new ArrayList<DirectoryStats>();
+		directoryStats = new HashMap<File, DirectoryStats>();
 	}
 	
 	@Override
@@ -30,19 +30,34 @@ public class Statistics extends MediaHandler {
 	
 	@Override
 	public void directoryInit(File directory) {
-		// TODO Auto-generated method stub
-
+		DirectoryStats dStats = new DirectoryStats(directory);
+		if (rootStats == null) {
+			rootStats = dStats;
+		}
+		directoryStats.put(directory, dStats);
 	}
 
 	@Override
 	public boolean handleFile(MediaFile f1) {
-		// TODO Auto-generated method stub
-		return false;
+		File f = f1.getBaseFile();
+		DirectoryStats cDir = directoryStats.get(f.getParentFile());
+		if (cDir == null) {
+			logger.warn("Unable to find stats for "+f.getParentFile());
+			cDir = new DirectoryStats(f.getParentFile());
+			directoryStats.put(f.getParentFile(), cDir);
+		} 
+		// Statistics doesn't apply any tags to the MediaFile, so we can just get the base File
+		return cDir.handleFile(f1.getBaseFile());
 	}
 
 	@Override
 	public void directoryComplete(File directory) {
-		// TODO Auto-generated method stub
+		DirectoryStats cDir = directoryStats.get(directory);
+		if (cDir == null) {
+			logger.error("Can't find stats for "+directory);
+		} else {
+			logger.info(cDir.report());
+		}
 	}
 
 	@Override
