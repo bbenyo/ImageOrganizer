a12878708c246b9def37da4d26bd65e75ac099a3
bbenyo <bbenyo@gmail.com>
2018-10-14 19:23:03 -0400

    Add stub for remove unreadable file handler

---
+++ src/main/java/util/handlers/RemoveUnreadable.java
@@ -0,0 +1,45 @@
+package util.handlers;
+
+import java.io.File;
+
+import util.struct.MediaFile;
+
+public class RemoveUnreadable extends MediaHandler {
+
+	@Override
+	public boolean fileFilter(MediaFile f1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void directoryInit(File directory) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void directoryComplete(File directory) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void subDirectoryInit(File directory, File subDirectory) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void subDirectoryComplete(File directory, File subDirectory) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean handleFile(MediaFile f1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
