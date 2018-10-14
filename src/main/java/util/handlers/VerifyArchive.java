15dda22a8e2f0056823bd9a8ac341f83f9547e88
bbenyo <bbenyo@gmail.com>
2018-10-14 19:09:54 -0400

    New handler to verify that a file is in the archive properly

---
+++ src/main/java/util/handlers/VerifyArchive.java
@@ -0,0 +1,45 @@
+package util.handlers;
+
+import java.io.File;
+
+import util.struct.MediaFile;
+
+public class VerifyArchive extends MediaHandler {
+
+	public VerifyArchive() {
+		super();
+	}
+	
+	@Override
+	public void directoryInit(File directory) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean handleFile(MediaFile f1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void directoryComplete(File directory) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean fileFilter(MediaFile f1) {
+		return true; 
+	}
+
+	@Override
+	public void subDirectoryInit(File directory, File subDirectory) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void subDirectoryComplete(File directory, File subDirectory) {
+		// TODO Auto-generated method stub
+	}
+
+}
