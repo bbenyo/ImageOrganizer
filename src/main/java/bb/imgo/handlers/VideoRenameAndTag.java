311ae6b2e2c58b5698275f0e5914e3e7888fe38a
bbenyo <bbenyo@gmail.com>
2019-01-01 18:27:31 -0500

    Another handler to be done, rename videos and tag via the user

---
+++ src/main/java/bb/imgo/handlers/VideoRenameAndTag.java
@@ -0,0 +1,26 @@
+package bb.imgo.handlers;
+
+import bb.imgo.struct.MediaFile;
+
+/** 
+ * Prompt the user to rename a video that is named ###.avi (no letters)
+ * Pop up a UI that lets the user play the video, rename, and tag (good/trash/archive)
+ * 
+ * @author Brett
+ *
+ */
+public class VideoRenameAndTag extends MediaHandler {
+
+	@Override
+	public boolean fileFilter(MediaFile f1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean handleFile(MediaFile f1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
